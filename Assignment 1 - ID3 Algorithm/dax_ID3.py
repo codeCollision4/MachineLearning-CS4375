5f50600e7ead93d7b7e731350e99756e039c55c2
codeCollision4 <79557266+codeCollision4@users.noreply.github.com>
2022-06-07 23:42:40 -0500

    Getting Input
    Starting to loop file and get data to feed to ID3

---
+++ Assignment 1 - ID3 Algorithm/dax_ID3.py
@@ -0,0 +1,30 @@
+import argparse
+from pathlib import Path
+
+
+def main():
+    
+    # Command Line Parser
+    parser = argparse.ArgumentParser()
+    
+    # Positional Arguments
+    parser.add_argument("train",
+                        help="Path to a .dat file that holds training data",
+                        type=Path
+                        )
+    parser.add_argument("test",
+                        help="Path to a .dat file that holds test data",
+                        type=Path
+                        )
+    # Parse input
+    args = parser.parse_args()
+    
+    # Getting training data from .dat file if path exists
+    if args.train.exists() and args.train.is_file():
+        # Looping thru file
+        with args.train.open() as f:
+            for idx, line in enumerate(f):
+                pass
+    else:
+        print("Please provide a .dat file that exists. Make sure you are not just providing a directory.")
+        exit()
